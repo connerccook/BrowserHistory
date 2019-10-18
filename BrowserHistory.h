f301c77adcb0da19cb1d58c08e325d1daf9dd4c6
gerarditooo9 <gerardo.robledo99@csu.fullerton.edu>
2019-10-18 13:02:00 -0700

    progess

--- BrowserHistory.h
+++ BrowserHistory.h
@@ -24,22 +24,54 @@ public:
 
 private:
   // TO BE COMPLETED
-  string fileName_;
+
   string url_;
   size_t filesize_;
-  string currentUrl_;
+
 };
 
 BrowserHistory::BrowserHistory() {
-  list<BrowserHistory> mylist;
   url_ = "";
   filesize_= 0;
-  currentUrl_ = url_;
+  list<BrowserHistory> mylist = {url_,filesize_};
+  list<BrowserHistory>::iterator it = mylist.begin(); //main iterator
 }
-BrowserHistory::visitSite(string url, size_t filesize){
+void BrowserHistory::visitSite(string url, size_t filesize){
   url_ = url;
   filesize_= filesize;
-  BrowserHistory
-  mylist.emplace_back()
-
+  BrowserHistory visited = {url_,filesize_};
+  mylist.emplace_back(visit); //back of the node is the most recent visit
+  if (it->url_== "") { //if browserHistory was empty, pop the "empty" node
+    mylist.pop_front();
+    it = mylist.begin();
+  } else {
+  it++; //iterator points to recent node
+}
+list<BrowserHistory>::iterator it2 = mylist.back(); //secondary iterator to delete history
+ while (it2!=it) { //if the main iterator is not in the back of the list, pop every node after
+   pop_back();
+   it2 = mylist.back();
+ }
+}
+void BrowserHistory::backButton() {
+if (it == mylist.begin()){} // if the iterator is at the beginning of the list, no change
+  else {
+  it--;
+}
+}
+void BrowserHistory::forwardButton() {
+  if (it == mylist.begin()) {} // if the iterator is at the beginning of the list, no change
+  else {
+    it++;
+  }
+}
+string BrowserHistory::currentUrl() {
+  string currentUrl = it -> url_;
+  return currentUrl;
+}
+void BrowserHistory::readFile(string fileName) {
+  ifstream fin(fileName);
+  string Url, function;
+  size_t fileSize;
+  
 }
